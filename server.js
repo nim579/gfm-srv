779f2eb6e1501a830b384361fd2759bca663023d
Nikolay Ivanushkin <nikolay.ivanushkin@inn.ru>
2014-05-17 18:17:18 +0400

    Supports node-srv@1.0.0, resolve #1

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var fs      = require('fs')
-  , program = require('commander')
-  , http    = require('http')
   , url     = require('url')
   , path    = require('path')
   , srv     = require('node-srv')
@@ -9,79 +7,55 @@ var fs      = require('fs')
   , render  = require('./renderer')
   , mdExts  = ['.md', '.markdown'];
 
-var gfm = srv;
+srv.prototype.addRequest = function(req, res){
+    reqObj = {
+        request: req,
+        response: res,
+        uid: _.random(0, 10000000),
+        startTime: new Date(),
+        uri: url.parse(req.url).pathname.replace(/^\//, '').replace(/\/$/, '/'+this.options.index),
+        body: ''
+    }
+    if(reqObj.uri.length === 0){
+        reqObj.uri = this.options.index;
+    }
 
-_.extend(gfm.prototype, {
-    addRequest: function(req, res){
-        reqObj = {
-            request: req,
-            response: res,
-            uid: _.random(0, 10000000),
-            startTime: new Date(),
-            uri: url.parse(req.url).pathname.replace(/^\//, '').replace(/\/$/, '/'+this.options.index),
-            body: ''
-        }
-        if(reqObj.uri.length === 0){
-            reqObj.uri = this.options.index;
-        }
+    if(_.compact(reqObj.uri.split(path.sep))[0] == 'static'){
+        reqObj.filename = path.resolve(process.cwd(), this.options.static ? this.options.static : '', reqObj.uri);
+    } else {
+        reqObj.filename = path.resolve(process.cwd(), this.options.root ? this.options.root : '', reqObj.uri);
+    }
 
-        if(_.compact(reqObj.uri.split(path.sep))[0] == 'static'){
-            reqObj.filename = path.resolve(process.cwd(), this.options.static ? this.options.static : '', reqObj.uri);
-        } else {
-            reqObj.filename = path.resolve(process.cwd(), this.options.root ? this.options.root : '', reqObj.uri);
-        }
+    this.stack.push(reqObj);
+    this.ev.emit('request');
+}
 
-        this.stack.push(reqObj);
-        this.ev.emit('request');
-    },
-    isMD: function(filepath){
-        return _.contains(mdExts, path.extname(filepath).toLowerCase())
-    },
-    startResponse: function(){
-        if(this.stack.length > 0){
-            var _this = this;
-            var reqObj = this.stack[0];
-            this.stack = _.without(this.stack, reqObj);
-
-            if(this.isMD(reqObj.filename)){
-                this.responseMD(reqObj, function(err, resObj){
-                    _this.response(resObj);
-                });
-            } else {
-                this.responseStatic(reqObj, function(err, resObj){
-                    _this.response(resObj);
-                });
-            }
-        }
-    },
-    responseMD: function(reqObj, callback){
-        var _this = this;
+srv.extendHandlers({
+    extnames: mdExts,
+    method: function(reqObj, callback){
+        var err = null;
         var filePath = reqObj.filename;
-        var fileName = path.basename(filePath, path.extname(filePath))
+        var fileName = path.basename(filePath, path.extname(filePath));
 
-        if(fs.existsSync(filePath)){
-            reqObj.mime = {"Content-Type": "text/html"};
-            reqObj.status = 200;
-            fs.readFile(filePath, {encoding: 'utf-8'}, function(err, file){
-                if(err){
-                    _this.response500(err, callback);
-                    this.errorLog(reqObj, 'Error 500: '+JSON.stringify(err));
-                    return false;
-                }
-                try {
-                    var readyFile = render(file);
-                    html = _.template(tmpl, {title: fileName.slice(0,1).toUpperCase() + fileName.slice(1).replace('_', ' '), content: readyFile.html, navigation: readyFile.navigation});
-                    reqObj.body = html;
+        reqObj.mime = {"Content-Type": "text/html"};
+        reqObj.status = 200;
+        fs.readFile(filePath, {encoding: 'utf-8'}, function(err, file){
+            if(err){
+                _this.response500(err, callback);
+                this.errorLog(reqObj, 'Error 500: '+JSON.stringify(err));
+                return false;
+            }
+            try {
+                var readyFile = render(file);
+                html = _.template(tmpl, {title: fileName.slice(0,1).toUpperCase() + fileName.slice(1).replace('_', ' '), content: readyFile.html, navigation: readyFile.navigation});
+                reqObj.body = html;
+            } catch(e) {
+                err = e;
+            }
 
-                    callback(null, reqObj);
-                } catch(e) {
-                    _this.response500(reqObj, e, callback);
-                }
-            });
-        } else {
-            this.response404(callback);
-        }
+            callback(err, reqObj);
+        });
     }
 });
 
-module.exports = gfm;
+module.exports = srv;
